dfe181ecae9c7b7aa3900042a205b134d22ededa
Amal <amal.mukhtar@outlook.com>
2022-07-25 19:55:29 +0100

    Game over function tweaked

--- script/app.js
+++ script/app.js
@@ -2,6 +2,7 @@ function init() {
   // * Elements
   const grid = document.querySelector(".grid");
   const cells = [];
+  const start = document.querySelector(".start");
 
   let snake = [2, 1, 0];
 
@@ -9,7 +10,7 @@ function init() {
   let snakeTimer = setInterval(moveSnake, 500);
   let snakeDirection = "right";
   let snakePosition = 10;
-  const appleTimer = setInterval(apple, 10000);
+  let appleTimer = setInterval(apple, 10000);
   let applePosition = 10;
   let score = null;
 
@@ -35,12 +36,11 @@ function init() {
   // * Execution
   // TODO: newCell/Direction
 
-  function getNewCell(e) {
+  function getNewCell() {
     if (snakeDirection === "left") {
       if (snakePosition % width === 0) {
         gameOver();
       }
-
       return snake[0] - 1;
     } else if (snakeDirection === "right") {
       if (snakePosition % width === width - 1) {
@@ -66,12 +66,9 @@ function init() {
 
   function moveSnake() {
     snake.forEach((element) => {
-      // ? element = num
-
       cells[element].classList.add("snakeImage");
     });
 
-    //Looks at the current state of direction + returns what the new head position should be
     let newCell = getNewCell();
 
     let tail = snake.pop();
@@ -81,11 +78,7 @@ function init() {
 
     cells[newCell].classList.add("snakeImage");
     snakePosition = newCell;
-    console.log(snakePosition);
-    // console.log("test", snake);
   }
-
-  moveSnake();
 
   // TODO: Apple
 
@@ -96,26 +89,21 @@ function init() {
   }
   apple();
 
+  // ! Error
+
   function appleEaten() {
-    if (cells[snakePosition].classList.contains("apple")) {
-      apple();
-      console.log("clicked");
+    if (
+      // cells[applePosition].classList.contains("apple") &&
+      // cells[snakePosition].classList.contains("snakeImages")
+      applePosition === snakePosition
+    ) {
+      console.log("same position");
       // ? Double check this
       snake.push(snake.length - 1 + 1);
+      apple();
     }
   }
   appleEaten();
-
-  // function gameOver() {
-  //   let confirm = window.confirm("game over - play again?");
-  //   if (confirm) {
-  //     moveSnake();
-  //   } else {
-  //     clearInterval(snakeTimer);
-  //     clearInterval(appleTimer);
-  //   }
-  // }
-  // gameOver();
 
   // TODO: Keypress event
   function handleKeyUp(e) {
@@ -136,9 +124,40 @@ function init() {
     } else {
       console.log("Invalid key");
     }
+
+    if (snakeDirection === "left") {
+      if (snakePosition % width === 0) {
+        console.log("left side");
+        gameOver();
+      }
+    } else if (snakeDirection === "right") {
+      if (snakePosition % width === width - 1) {
+        console.log("right side");
+        gameOver();
+      }
+    } else if (snakeDirection === "up") {
+      if (snakePosition <= width) {
+        console.log("up side");
+        gameOver();
+      }
+    } else if (snakeDirection === "down") {
+      if (snakePosition + width >= cellCount) {
+        console.log("down side");
+        gameOver();
+      }
+    } else {
+      // ? Check this
+      return;
+    }
   }
 
   // TODO: Game over
+
+  function gameOver() {
+    clearInterval(snakeTimer);
+    clearInterval(appleTimer);
+    window.alert("wall hit");
+  }
 
   //* Events
   document.addEventListener("keyup", handleKeyUp);
